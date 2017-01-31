83b1ce71e1127195a90551b62b87dfbd37fe7e72
Unknown <rafaelt91@gmail.com>
2017-01-31 12:21:48 -0300

    Access_token is passed as a parameter of OIDC.getUserInfo

--- openidconnect.js
+++ openidconnect.js
@@ -947,13 +947,12 @@ OIDC.discover = function(issuer)
     }
 };
 
-OIDC.getUserInfo = function()
+OIDC.getUserInfo = function(access_token)
 {
   try {
       var currentURL = window.location.href;
       var providerURL = currentURL.match('https://([^/]*)')
       var providerInfo = OIDC.discover(providerURL[0]);
-      var access_token = OIDC.getAccessToken();
       var request = new XMLHttpRequest();
       request.open('GET', providerInfo['userinfo_endpoint'], false);
       request.setRequestHeader("authorization", "Bearer " + access_token);
