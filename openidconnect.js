2b3b20eaa998b64a88b0f230dddbcc2bcd7c3b3a
David Rutsch <daverutsch@gmail.com>
2018-04-12 17:04:28 -0500

    debug

--- openidconnect.js
+++ openidconnect.js
@@ -891,6 +891,8 @@ OIDC.getValidIdToken = function(validationOptions)
         var storedState = sessionStorage['state'];
         var goodState = customValidatorExists ? validationOptions['validator'](urlState, storedState) : urlState === storedState;
 
+        console.log({ goodState: goodState, urlState: urlState, storedState: storedState, customValidatorExists: customValidatorExists })
+
         // Extract id token from the id_token parameter
         var match = url.match('[?#&]id_token=([^&]*)');
         if (!goodState) {
